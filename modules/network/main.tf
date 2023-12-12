9a04925aeaaf1ef98d978d01de1ee009a2703c30
Liam Stevens <8955671+liamstevens111@users.noreply.github.com>
2023-12-12 23:17:11 +0700

    Initial

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -85,6 +85,24 @@ resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.namespace}-private-routing-table"
+  }
+}
+
+resource "aws_route_table_association" "private_1" {
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private_1.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private_2.id
+}
+
 resource "aws_security_group" "alb_main" {
   name        = "alb_security_group"
   description = "Security group for ALB"
